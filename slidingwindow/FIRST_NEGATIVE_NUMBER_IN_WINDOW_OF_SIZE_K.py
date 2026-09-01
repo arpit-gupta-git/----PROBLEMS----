e2506d49c66b38124c9bc64947db8b8b4293bbba
arpit-gupta-git <argupta1098@gmail.com>
2026-09-01 14:50:56 +0530

    Update FIRST_NEGATIVE_NUMBER_IN_WINDOW_OF_SIZE_K.py
    Solved in GFG

--- slidingwindow/FIRST_NEGATIVE_NUMBER_IN_WINDOW_OF_SIZE_K.py
+++ slidingwindow/FIRST_NEGATIVE_NUMBER_IN_WINDOW_OF_SIZE_K.py
@@ -1,1 +1,15 @@
-
+from collections import deque
+class Solution:
+    def firstNegInt(self, arr, k):
+        result = []
+        q = deque()
+        l =0 
+        for i in range(len(arr)):
+            if arr[i] < 0 :
+                q.append(arr[i])
+            if i - l + 1 == k :
+                result.append(q[0]) if q else result.append(0)
+                if arr[l] < 0 :
+                    q.popleft()
+                l += 1 
+        return result
